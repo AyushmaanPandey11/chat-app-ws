16f06bcec622720b206f55f4e2570c59076def14
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2025-06-27 02:56:08 +0530

    responsiveness for mob

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -173,26 +173,34 @@ function App() {
   }, []);
 
   return (
-    <div className="h-screen bg-black flex justify-center items-center">
+    <div className="h-screen bg-black flex justify-center items-center overscroll-contain">
       <div
-        className={`flex flex-col justify-center items-center mx-auto border-white border-2 rounded-2xl w-11/12 sm:w-1/3 xl:w-1/3 lg:w-2/5 ${
+        className={`flex flex-col justify-center items-center mx-auto border-white border-2 rounded-2xl w-11/12 sm:w-2/3 xl:w-1/3 lg:w-2/5 ${
           data.code !== "" ? "h-5/6" : "h-3/5 sm:h-3/5 lg:h-1/3"
         }`}
       >
         <div className="mx-auto flex h-1/12 sm:h-2/12 w-full text-white justify-center mb-4 sm:mb-4 lg:mb-0">
           <div className="flex flex-col p-4 m-2 w-10/12 space-y-0 sm:space-y-2">
             <div className="text-white flex flex-col justify-center">
-              <span className="font-bold text-left text-[2vw] sm:text-[3vw] lg:text-xl mb-0 sm:mb-1 lg:mb-0">
+              <span
+                className={`font-bold text-left sm:text-[3vw] lg:text-xl sm:mb-1 lg:mb-0 ${
+                  data.code == "" ? "text-3xl mb-1" : "text-[2.5vw]"
+                }`}
+              >
                 Real Time Chat
               </span>
-              <span className="font-bold text-left sm:text-[1.5vw] text-[1.5vw] lg:text-xs text-white">
+              <span
+                className={`font-bold text-left sm:text-[1.5vw] lg:text-xs text-white ${
+                  data.code == "" ? "text-lg" : "text-[2vw]"
+                } `}
+              >
                 {data.code !== ""
                   ? "Tell Your Friends to join the room!!"
                   : "Unsaved Chat between users using WebSockets"}
               </span>
             </div>
             {data.code !== "" && (
-              <div className="flex flex-row justify-between bg-gray-700 rounded-md p-1 font-bold text-[0.5rem] lg:text-[0.8rem]">
+              <div className="flex flex-row justify-between bg-gray-700 rounded-md mt-1 p-1 font-bold text-[0.5rem] lg:text-[0.8rem]">
                 <p>Room Code: {data.code}</p>
                 <p>Active Users: {data.users}</p>
               </div>
@@ -200,7 +208,7 @@ function App() {
           </div>
         </div>
 
-        <div className="mx-auto flex h-10/12 sm:h-9/12 w-full text-white py-1 p-3 m-1 overflow-y-visible mt-3 md:mt-5 lg:mt-4">
+        <div className="mx-auto flex h-10/12 sm:h-9/12 w-full text-white py-1 p-3 m-1 overflow-y-visible mt-2 md:mt-5 lg:mt-4">
           {data.code !== "" ? (
             <div className="flex flex-col justify-start mx-auto border-2 border-white rounded-xl p-4 pb-0 m-2 w-10/12 items-start space-y-2 overflow-y-auto">
               {messages.messages.map((msg, idx) =>
@@ -222,7 +230,7 @@ function App() {
           ) : (
             <div className="flex justify-center items-center w-full h-full mt-0 sm:mt-0 lg:mt-4">
               <div
-                className="bg-gray-200 text-black hover:cursor-pointer w-10/12 flex justify-center items-center rounded-xl sm:text-[3.3vw] text-3xl lg:text-xl 2xl:text-2xl font-bold sm:p-3 p-3 lg:p-3 2xl:p-6"
+                className="bg-gray-200 text-black hover:cursor-pointer w-10/12 flex justify-center items-center rounded-xl sm:text-[3.3vw] text-3xl lg:text-xl 2xl:text-2xl font-bold sm:p-3 p-7 lg:p-3 2xl:p-6"
                 onClick={handleCreateRoom}
               >
                 Create New Room
@@ -232,7 +240,7 @@ function App() {
         </div>
 
         <div className="w-full flex justify-center items-center flex-row text-white 2xl:mt-2">
-          <div className="flex lg:flex-row flex-col sm:flex-col w-11/12 lg:w-10/12 lg:mb-6 mb-2 sm:mb-2 px-1">
+          <div className="flex lg:flex-row flex-col sm:flex-col w-11/12 lg:w-10/12 lg:mb-6 mb-2 sm:mb-2 px-1 sm:pl-4">
             <div className="flex lg:flex-row flex-col sm:flex-col w-full">
               <input
                 ref={inputRef}
